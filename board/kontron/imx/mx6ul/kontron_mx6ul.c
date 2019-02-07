863a7c25ac96bdfc074d9884fb3d0a160c0ac7f7
Frieder Schrempf <frieder.schrempf@kontron.de>
2019-02-07 17:42:24 +0100

    imx: imx6ul: Add support for Kontron Electronics i.MX6UL/ULL boards N63xx/N4xx
    This adds support for i.MX6UL/ULL-based evaluation kits with SOMs by
    Kontron Electronics GmbH.
    
    Currently there are the following SOM flavors:
      * N6310: SOM with i.MX6UL-2, 256MB RAM, 256MB SPI NAND
      * N6311: SOM with i.MX6UL-2, 512MB RAM, 512MB SPI NAND
      * N6411: SOM with i.MX6ULL, 512MB RAM, 512MB SPI NAND
    
    And the according evaluation boards:
      * N6310-S: Baseboard with SOM N6310, eMMC, display (optional), ...
      * N6311-S: Baseboard with SOM N6311, eMMC, display (optional), ...
      * N6411-S: Baseboard with SOM N6411, eMMC, display (optional), ...
    
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>

---
+++ board/kontron/imx/mx6ul/kontron_mx6ul.c
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2018 Kontron Electronics GmbH
+ */
+
+#include <asm/arch/clock.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/mx6-pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/io.h>
+#include <common.h>
+#include <fsl_esdhc.h>
+#include <miiphy.h>
+#include <linux/sizes.h>
+#include <mmc.h>
+#include <netdev.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+	gd->ram_size = imx_ddr_size();
+
+	return 0;
+}
+
+static int setup_fec(void)
+{
+	struct iomuxc *const iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
+	int ret;
+
+	/*
+	 * Use 50M anatop loopback REF_CLK1 for ENET1,
+	 * clear gpr1[13], set gpr1[17].
+	 */
+	clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
+			IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
+
+	/*
+	 * Use 50M anatop loopback REF_CLK2 for ENET2,
+	 * clear gpr1[14], set gpr1[18].
+	 */
+	clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC2_MASK,
+			IOMUX_GPR1_FEC2_CLOCK_MUX1_SEL_MASK);
+
+	ret = enable_fec_anatop_clock(0, ENET_50MHZ);
+	if (ret)
+		return ret;
+
+	ret = enable_fec_anatop_clock(1, ENET_50MHZ);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+int board_phy_config(struct phy_device *phydev)
+{
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1f, 0x8190);
+
+	if (phydev->drv->config)
+		phydev->drv->config(phydev);
+
+	return 0;
+}
+
+int board_early_init_f(void)
+{
+	enable_qspi_clk(0);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* Address of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM + 0x100;
+
+	setup_fec();
+
+	return 0;
+}
